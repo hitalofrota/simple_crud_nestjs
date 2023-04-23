1ea1a0055906f7c7c0f29824f7578356106e58f7
hitalo <hitalo.frota@tallos.com.br>
2023-04-23 00:11:59 -0300

    feat-implementação do orm com o banco de dados

--- dist/tasks/shared/task.service/task.service.js
+++ dist/tasks/shared/task.service/task.service.js
@@ -5,60 +5,43 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TaskService = void 0;
 const common_1 = require("@nestjs/common");
+const mongoose_1 = require("@nestjs/mongoose");
+const mongoose_2 = require("mongoose");
 let TaskService = class TaskService {
-    constructor() {
-        this.tasks = [
-            { id: 1, description: 'Tarefa 1', completed: false },
-            { id: 2, description: 'Tarefa 2', completed: false },
-            { id: 3, description: 'Tarefa 3', completed: true },
-            { id: 4, description: 'Tarefa 4', completed: false },
-            { id: 5, description: 'Tarefa 5', completed: false },
-            { id: 6, description: 'Tarefa 6', completed: false },
-            { id: 7, description: 'Tarefa 7', completed: false },
-            { id: 8, description: 'Tarefa 8', completed: false },
-            { id: 9, description: 'Tarefa 9', completed: false },
-            { id: 10, description: 'Tarefa 10', completed: false },
-        ];
+    constructor(taskModel) {
+        this.taskModel = taskModel;
     }
-    getAll() {
-        return this.tasks;
+    async getAll() {
+        return await this.taskModel.find().exec();
     }
-    getById(id) {
-        const task = this.tasks.find((value) => value.id == id);
-        return task;
+    async getById(id) {
+        return await this.taskModel.findById(id).exec();
     }
-    create(task) {
-        console.log("Aqui", task);
-        let lastId = 0;
-        if (this.tasks.length > 0) {
-            lastId = this.tasks[this.tasks.length - 1].id;
-        }
-        task.id = lastId + 1;
-        this.tasks.push(task);
-        return task;
+    async create(task) {
+        const createdTask = new this.taskModel(task);
+        return await createdTask.save();
     }
-    update(task) {
-        const taskArray = this.getById(task.id);
-        if (taskArray) {
-            taskArray.description = task.description;
-            taskArray.completed = task.completed;
-        }
-        else {
-            taskArray.description = "Deu ruim meu patrão";
-            return;
-        }
-        return taskArray;
+    async update(id, task) {
+        await this.taskModel.updateOne({ _id: id }, task).exec();
+        return this.getById(id);
     }
-    delete(id) {
-        const index = this.tasks.findIndex((value) => value.id == id);
-        this.tasks.splice(index, 1);
+    async delete(id) {
+        return await this.taskModel.deleteOne({ _id: id }).exec();
     }
 };
 TaskService = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __param(0, (0, mongoose_1.InjectModel)('Task')),
+    __metadata("design:paramtypes", [mongoose_2.Model])
 ], TaskService);
 exports.TaskService = TaskService;
 //# sourceMappingURL=task.service.js.map
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://hitalofrotadoidao:hitalofrotadoidao@clusterzinho.p7cyrlh.mongodb.net/?retryWrites=true&w=majority'),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
